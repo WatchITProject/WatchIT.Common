220517351a9986087d358a1cd6294f4fa3d5e95c
Mateusz Skoczek <mateusz5432160@gmail.com>
2023-11-17 19:12:33 +0100

    RegisterRequest added

---
+++ WatchIT.Common/Accounts/Request/RegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WatchIT.Common.Accounts.Request
+{
+    public class RegisterRequest
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+    }
+}
