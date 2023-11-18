767e84d82f125144a79a2441457c13ec44e8d864
Mateusz Skoczek <mateusz5432160@gmail.com>
2023-11-18 11:43:45 +0100

    AuthenticateResponse added

---
+++ WatchIT.Common/Accounts/Response/AuthenticateResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WatchIT.Common.Accounts.Response
+{
+    public class AuthenticateResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("refresh_token")]
+        public string RefreshToken { get; set; }
+    }
+}
