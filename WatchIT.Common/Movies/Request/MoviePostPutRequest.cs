1dbd43d40056838841c6f5ed27ebe79720b070b6
Mateusz Skoczek <mateusz5432160@gmail.com>
2023-11-19 20:01:03 +0100

    Request/Response models added

---
+++ WatchIT.Common/Movies/Request/MoviePostPutRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WatchIT.Common.Movies.Request
+{
+    public class MoviePostPutRequest
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("original_title")]
+        public string OriginalTitle { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("release_date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [JsonPropertyName("length")]
+        public short Length { get; set; }
+    }
+}
