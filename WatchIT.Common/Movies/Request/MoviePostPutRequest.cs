dc0e839579094eacb4a381ee1c6315fd91f099c7
Mateusz Skoczek <mateusz5432160@gmail.com>
2023-11-19 20:54:29 +0100

    Change in request

--- WatchIT.Common/Movies/Request/MoviePostPutRequest.cs
+++ WatchIT.Common/Movies/Request/MoviePostPutRequest.cs
@@ -13,10 +13,10 @@ namespace WatchIT.Common.Movies.Request
         public string Title { get; set; }
 
         [JsonPropertyName("original_title")]
-        public string OriginalTitle { get; set; }
+        public string? OriginalTitle { get; set; }
 
         [JsonPropertyName("description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [JsonPropertyName("release_date")]
         public DateOnly ReleaseDate { get; set; }
